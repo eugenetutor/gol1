3b82f71e97f595c309eb5490f8adde5d4311f813
Eugene Demenko <eugenekiberone@gmail.com>
2025-04-26 14:54:27 +0300

    data type

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
-func main(){
-	const Pi = 3.14159
-	const MaxUsers = 1000
+func main() {
+	var a int8 = -128
+	fmt.Println("int8: ", a, "size of:", unsafe.Sizeof(a), "byte")
+	var b int16 = 32767
+	fmt.Println("int16: ", b, "size of:", unsafe.Sizeof(b), "byte")
+	var c int64 = 999999999999999999
+	fmt.Println("int64: ", c, "size of:", unsafe.Sizeof(c), "byte")
+	
+	var d float32 = 3.14
+	fmt.Println("float32: ", d, "size of:", unsafe.Sizeof(d), "byte")
 
-	const (
-		StatusOK = 200
-		StatusNotFound = 404
-		StatusInternalErro = 500
-	)
-
-	StatusOK = 300//error
-	fmt.Println(StatusOK)
+	var str string = "Hello worldefefefefsdvd wefefefefasdfasfsa "
+	fmt.Println("string: ", str, "size of:", unsafe.Sizeof(str), "byte")
 }
