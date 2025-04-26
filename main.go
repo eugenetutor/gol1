e2eeb444f5a278624284dd80894f5118b153803d
Eugene Demenko <eugenekiberone@gmail.com>
2025-04-26 14:27:50 +0300

    use :=

--- main.go
+++ main.go
@@ -3,23 +3,14 @@ package main
 import "fmt"
 
 func main(){
-	var name string
-	var age int 
 
-	var message string = "Hello"
-	var count int  = 42
+	value := 10
+	if true {
+		value := 20 //нова змінна
+		fmt.Println(value)
+	}
 
-	var greeting = "Good Morning"//string
-	var number = 100 // int
+	fmt.Println(value)
 
-	var (
-		firstName string  = "Taras"
-		lastName string  = "Shevchenko"
-		year int = 1814
-	)
 
-	var a, b, c int
-
-	fmt.Print("hello world")
-	fmt.Print(name)
 }
