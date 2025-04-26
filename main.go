57f39915e7d0accacdbfd610b44a358d4f96fa07
Eugene Demenko <eugenekiberone@gmail.com>
2025-04-26 14:21:31 +0300

    declared var

--- main.go
+++ main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main(){
+	var name string
+	var age int 
+
+	var message string = "Hello"
+	var count int  = 42
+
+	var greeting = "Good Morning"//string
+	var number = 100 // int
+
+	var (
+		firstName string  = "Taras"
+		lastName string  = "Shevchenko"
+		year int = 1814
+	)
+
+	var a, b, c int
+
 	fmt.Print("hello world")
+	fmt.Print(name)
 }
