a0932db322b1cc040ccb91e30b7225fa62cc4afe
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-03 14:31:00 +0300

    switch with type

--- main.go
+++ main.go
@@ -2,32 +2,19 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	// switch вираз {
-	// case знач1:
-	// 		//код1
-	// case знач2:
-	// 		//код2
-	// case знач3:
-	// 		//код3
-	// default:
-	// 		//код, якщо нічого не підішло
-	// }
-
-	rating := 4
-	switch rating{
-	case 5:
-		fmt.Println("Дякуємо щиро за ваг відгук!")
-	case 4:
-		fmt.Println("Дякуємо за високу оцінку!")
-	case 3:
-		fmt.Println("Дякуємо! Ми працюємо над покращенням!")
-	case 1,2:
-		fmt.Println("Нам шкода, що ви залишилися незадоволеними")
-	default:
-		fmt.Println("Некоректна оцінка")
+	var value interface{} = 63.3
+	switch v := value.(type){
+	case string: 
+		fmt.Println("String:", v)
+	case int: 
+		fmt.Println("Int:", v)
+	case float64: 
+		fmt.Println("Float64:", v)
+	default: 
+		fmt.Println("Other type")
 	}
+	
 }
