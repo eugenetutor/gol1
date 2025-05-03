9e8aab322f6d8dce1f4872c348fa8e319dfa7faa
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-03 13:53:43 +0300

    if else construction

--- main.go
+++ main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	age := 16
-	isAdult := age >= 18
-	hasPermission := IsLoggedIn && hasRole
-	canAccess := isAdmin || hasPermission
-	isRestricted := !canAccess
+	currentHour := time.Now().Hour()
+	fmt.Println(currentHour)
+	if currentHour >= 6 && currentHour < 22{
+		fmt.Println("Зараз день")
+	}else{
+		fmt.Println("Зараз ніч")
+	}
+
+	if rand.Intn(2) == 0{
+		fmt.Println("Орел")
+	}else{
+		fmt.Println("решка")
+	}
+
+	statusCode := 200
+	if statusCode >= 200 && statusCode < 300{
+		fmt.Println("Ok")
+	}else{
+		fmt.Println("no Ok")
+	}
 }
