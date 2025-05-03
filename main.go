8b32b9f3a5d900b0f4fb8a4cc295587d01fc97b2
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-03 14:06:40 +0300

    n2

--- main.go
+++ main.go
@@ -2,21 +2,19 @@ package main
 
 import (
 	"fmt"
-	"path/filepath"
+	"time"
 )
 
 func main() {
-	fileName := "document.pdf"
-	extension := filepath.Ext(fileName)
-	if extension == ".jpg" || extension == ".png" || extension == ".gif"{
-		fmt.Println("It's picture")
-	}else if extension == ".mp4" || extension == ".mov" || extension == ".avi"{
-		fmt.Println("It's video")
-	}else if extension == ".mp3" || extension == ".wav" || extension == ".flac"{
-		fmt.Println("It's music")
-	}else if extension == ".doc" || extension == ".pdf" || extension == ".txt"{
-		fmt.Println("It's document")
-	}else {
-		fmt.Println("Unknown")
+	// if змінна := вираз; умова{
+	// 	 істина
+	// }
+
+	if today := time.Now().Weekday(); today == time.Saturday || today == time.Sunday {
+		fmt.Println("Це вихідний")
+	} else if today == time.Friday{
+		fmt.Println("Пʼятниця! Скоро вихідні")
+	}else{
+		fmt.Println("Робочий день :((", today)
 	}
 }
