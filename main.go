c6f93a4d439d4b9749ae5e0d8840dd12b2642853
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-03 13:26:46 +0300

    added operation

--- main.go
+++ main.go
@@ -2,31 +2,14 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	a := 10
-	b := 3
-
-	sum := a + b
-	fmt.Printf("%d + %d = %d\n", a, b, sum)
-
-	dif := a - b
-	fmt.Printf("%d - %d = %d\n", a, b, dif)
-
-	prod := a * b
-	fmt.Printf("%d * %d = %d\n", a, b, prod)
-
-	quo := a / b
-	fmt.Printf("%d / %d = %d\n", a, b, quo)
-
-	rem := a % b
-	fmt.Printf("%d %% %d = %d\n", a, b, rem)
-
-	floatQuo := float64(a) / float64(b)
-	fmt.Printf("%d / %d = %.1f\n", a, b, floatQuo)
-
-	fmt.Print(math.Pow(float64(a), 2))
-
+	a, b := 10, 20
+	fmt.Println(a == b)
+	fmt.Println(a != b)
+	fmt.Println(a < b)
+	fmt.Println(a > b)
+	fmt.Println(a <= b)
+	fmt.Println(a >= b)
 }
