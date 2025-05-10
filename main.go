4f379ece2353f744701c01a4b079b066c1a44deb
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-10 14:35:58 +0300

    bytes in go. stage 1

--- main.go
+++ main.go
@@ -5,51 +5,9 @@ import (
 )
 
 func main() {
-	// for...range
-	marks := map[string]int{
-		"Alex": 20,
-		"Nikita": 50,
-		"Andrew": 30,
-		"Yevhenii": 2,
-	}
-
-	for name, mark := range marks{
-		fmt.Printf("%s: %d\n", name, mark)
-	}
-	
-	for name := range marks{
-		fmt.Println("Name: ", name)
-		if name == "Alex"{
-			break
-		}
-	}
-	
-	outerLoop:
-	for i := 0; i<3; i++{
-		for j:=0; j<3; j++{
-			if i == 2 && j == 2{
-				fmt.Println("Переривання зовнішнього циклу")
-				break outerLoop
-			}
-			fmt.Printf("i: %d, j: %d\n", i,j)
-		}
-	}
-
-	for i := 0; i<10; i++{
-		if i % 2 != 0{
-			continue
-		}
-		fmt.Println("Парне число", i)
-	}
-
-	for i := 2; i < 10; i++ {
-		
-		if 21%i == 0 {
-			fmt.Println(i)
-		}
-		if i == 9 {
-			fmt.Println(i)
-		}
-	}
+	var ch byte = 65
+	var ch1 byte = 0b1000001
+	var ch2 byte = 0o101
+	var ch3 byte = 0x41
 
 }
