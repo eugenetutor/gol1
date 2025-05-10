99fda8970534ef62242f714225c7915d3faa8f1c
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-10 14:54:13 +0300

    rune

--- main.go
+++ main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"fmt"
+	"unicode/utf8"
 )
 
 func main() {
-	var ch byte = 65
-	var ch1 byte = 0b1000001
-	var ch2 byte = 0o101
-	var ch3 byte = 0x41
+	text := "Привіт"
+	fmt.Println(len(text))
+	for i:=0; i< len(text); i++{
+		fmt.Printf("Байт %d: %d\n", i, text[i])
+	}
 
+	fmt.Println(utf8.RuneCountInString(text))
+
+	//rune - псевдонім для типу int32.Unicode символи
+
+	var runa2 rune = '人'
+	fmt.Printf("тип:%T, значення:%v\n", runa2, runa2)
+
+	var symbol rune = '世'
+	fmt.Printf("Symbol: %c\n", symbol)
+	fmt.Printf("Symbol: %d\n", symbol)
+	fmt.Printf("Symbol: %U\n", symbol)
+	fmt.Printf("Symbol: %T\n", symbol)
+
+	var symbol2 rune = '🗿'
+	fmt.Printf("Symbol: %c\n", symbol2)
+	fmt.Printf("Symbol: %d\n", symbol2)
+	fmt.Printf("Symbol: %U\n", symbol2)
+	fmt.Printf("Symbol: %T\n", symbol2)
 }
