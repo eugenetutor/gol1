505fd290fe3e4fc0d20dd0d5170a478efc76b680
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-10 13:32:32 +0300

    loop stage 1

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	currentHour := time.Now().Hour()
-	fmt.Println(currentHour)
-	if currentHour >= 6 && currentHour < 22{
-		fmt.Println("Зараз день")
-	}else{
-		fmt.Println("Зараз ніч")
+	// for ініціалізація; умова; лічильник{
+	// 	тіло циклу
+	// }
+	var a int
+	for i := 0; i <= 3; i++ {
+		a = a + 2
+		fmt.Printf("Iteration %d %d \n", i, a)
 	}
+	fmt.Println(a)
 }
