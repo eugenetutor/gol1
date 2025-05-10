a43cd0cf133e91231b1e3ad7709bbd726dac66d5
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-10 13:48:20 +0300

    loop stage v2

--- main.go
+++ main.go
@@ -5,13 +5,14 @@ import (
 )
 
 func main() {
-	// for ініціалізація; умова; лічильник{
-	// 	тіло циклу
-	// }
-	var i int = 0
-	for i <= 3{
-		fmt.Printf("Iteration %d %d \n", i)
-		i++
+	// for...range
+	names := []string{"Alex", "Nikita", "Andrew", "Yevhenii"}
+
+	for index, name := range names{
+		fmt.Printf("Index %d: %s\n", index, name)
 	}
-	
+
+	for _, name := range names{
+		fmt.Printf("Name: %s\n", name)
+	}
 }
