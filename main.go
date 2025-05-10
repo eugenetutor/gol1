4d0d00b98bd637c6bddd7bd1db4d6ef0a8f6880d
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-10 14:31:17 +0300

    loop. stage v3

--- main.go
+++ main.go
@@ -6,13 +6,50 @@ import (
 
 func main() {
 	// for...range
-	names := []string{"Alex", "Nikita", "Andrew", "Yevhenii"}
-
-	for index, name := range names{
-		fmt.Printf("Index %d: %s\n", index, name)
+	marks := map[string]int{
+		"Alex": 20,
+		"Nikita": 50,
+		"Andrew": 30,
+		"Yevhenii": 2,
 	}
 
-	for _, name := range names{
-		fmt.Printf("Name: %s\n", name)
+	for name, mark := range marks{
+		fmt.Printf("%s: %d\n", name, mark)
 	}
+	
+	for name := range marks{
+		fmt.Println("Name: ", name)
+		if name == "Alex"{
+			break
+		}
+	}
+	
+	outerLoop:
+	for i := 0; i<3; i++{
+		for j:=0; j<3; j++{
+			if i == 2 && j == 2{
+				fmt.Println("Переривання зовнішнього циклу")
+				break outerLoop
+			}
+			fmt.Printf("i: %d, j: %d\n", i,j)
+		}
+	}
+
+	for i := 0; i<10; i++{
+		if i % 2 != 0{
+			continue
+		}
+		fmt.Println("Парне число", i)
+	}
+
+	for i := 2; i < 10; i++ {
+		
+		if 21%i == 0 {
+			fmt.Println(i)
+		}
+		if i == 9 {
+			fmt.Println(i)
+		}
+	}
+
 }
