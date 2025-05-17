506684f62efb8d0229d7cbd7d988067a64f4f8ed
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-17 15:01:42 +0300

    defer 1

--- main.go
+++ main.go
@@ -4,53 +4,18 @@ import (
 	"fmt"
 )
 
-func incrementGenerator() func() int {
-	count := 0
-
-	return func() int {
-		count++
-		return count
-	}
-}
-
 func main() {
-	increment := incrementGenerator()
+	demoDefer()
+}
 
-	fmt.Println(increment())
-	fmt.Println(increment())
-	fmt.Println(increment())
+func demoDefer(){
+	//LIFO - (Last in, First out)
+	defer fmt.Println("Last output")
+	defer fmt.Println("Second output")
 
-	anotherIncrement := incrementGenerator()
-
-	fmt.Println(anotherIncrement())
-	fmt.Println(anotherIncrement())
-	fmt.Println(anotherIncrement())
-
-	double := multiplyBy(2)
-	triple := multiplyBy(3)
-
-	fmt.Println(double(5))
-	fmt.Println(triple(5))
-
-	//example 3
-	yourAccount := createBankAccount(1000)
-	fmt.Println(yourAccount(500))
-	fmt.Println(yourAccount(-200))
+	fmt.Println("First output")
 }
 
 
-func multiplyBy(multiplier int) func(int) int {
-	 return func(value int) int {
-		return value * multiplier
-	 }
-}
-
-func createBankAccount(initBalance float64) func(float64) float64{
-	balance := initBalance
-	return func(sum float64) float64{
-		balance += sum
-		return balance
-	}
-}
 
 
