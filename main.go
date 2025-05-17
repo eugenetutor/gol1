d8046bf0118a89cb1c3da495cbf5301cf4c04a40
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-17 14:07:24 +0300

    func lvl2

--- main.go
+++ main.go
@@ -1,22 +1,48 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func output(input int) {
-	fmt.Println("========")
-	fmt.Println(&input)
-	fmt.Println(input)
-}
-
 func main() {
-	a := 2
-	fmt.Println(&a)
-	output(a)
+
+	q,r := divideWithRemainder(13,5)
+	fmt.Printf("13 / 5 = %d with remainder %d\n", q, r)
+	number, _, _ := getData()
+	fmt.Println("Number: ", number)
+
+	_, _, isValid := getData()
+	fmt.Println("Correct: ", isValid)
+
+	result, err := divide(10,2)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}else {
+		fmt.Println("Result: ", result)
+	}
+
+	result, err = divide(10,0)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}else {
+		fmt.Println("Result: ", result)
+	}
 }
 
-// func functionName(par1 type1, par2 type2, ...) return_type{
-// 	    //function body
-// 	    return value
-// }
+func divide(a,b float64) (float64, error){
+	if b == 0 {
+		return 0, errors.New("ділення на нуль")
+	}
+	return a / b, nil
+}
+
+func divideWithRemainder(a,b int) (int, int) {
+	q := a/b
+	r := a%b
+	return q,r
+}
+
+func getData() (int, string, bool) {
+	return 42, "hello", true
+}
