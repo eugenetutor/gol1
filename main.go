8e32e7760b5885f3f720125cb74d89cea4f008c1
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-17 14:58:55 +0300

    closure

--- main.go
+++ main.go
@@ -4,20 +4,53 @@ import (
 	"fmt"
 )
 
-func main() {
-	x := 20
-	func(){
-		x = 25
-		fmt.Println("Hello from anonymous func", x)
-	}()
+func incrementGenerator() func() int {
+	count := 0
 
-	func(name string){
-		fmt.Println("Hello from anonymous func", name)
-	}("Golang")
-
-	square := func(n int) int {
-		return n * n
+	return func() int {
+		count++
+		return count
 	}
-
-	fmt.Println(square(8))
 }
+
+func main() {
+	increment := incrementGenerator()
+
+	fmt.Println(increment())
+	fmt.Println(increment())
+	fmt.Println(increment())
+
+	anotherIncrement := incrementGenerator()
+
+	fmt.Println(anotherIncrement())
+	fmt.Println(anotherIncrement())
+	fmt.Println(anotherIncrement())
+
+	double := multiplyBy(2)
+	triple := multiplyBy(3)
+
+	fmt.Println(double(5))
+	fmt.Println(triple(5))
+
+	//example 3
+	yourAccount := createBankAccount(1000)
+	fmt.Println(yourAccount(500))
+	fmt.Println(yourAccount(-200))
+}
+
+
+func multiplyBy(multiplier int) func(int) int {
+	 return func(value int) int {
+		return value * multiplier
+	 }
+}
+
+func createBankAccount(initBalance float64) func(float64) float64{
+	balance := initBalance
+	return func(sum float64) float64{
+		balance += sum
+		return balance
+	}
+}
+
+
