beec106e7177505842bc50a9a8cf18a767c49669
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-17 14:43:51 +0300

    func lvl5

--- main.go
+++ main.go
@@ -5,15 +5,19 @@ import (
 )
 
 func main() {
-	fmt.Printf("String %d %d \n", 2, 4)
-	fmt.Println(sum(1,2))
-	fmt.Println(sum(1,2,3,3,4,4,5,5,5,6,6,7,7,7,8,8,8,98,9,8,7,5,4,3,2,4,5,6,7,7,6,5,4,3,3,4,5,56,6,7))
-}
+	x := 20
+	func(){
+		x = 25
+		fmt.Println("Hello from anonymous func", x)
+	}()
 
-func sum(numbers ...int) int {
-	total := 0
-	for _, num := range numbers{
-		total += num
+	func(name string){
+		fmt.Println("Hello from anonymous func", name)
+	}("Golang")
+
+	square := func(n int) int {
+		return n * n
 	}
-	return total
+
+	fmt.Println(square(8))
 }
