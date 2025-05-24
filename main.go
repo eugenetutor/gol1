c47b6df9dc8d980ebaefeecb2c6322468a380cc1
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-24 14:08:53 +0300

    collection. slice. pt2

--- main.go
+++ main.go
@@ -5,23 +5,19 @@ import (
 )
 
 func main() {
-	//масиви - це нумерований послідовність  елементів ОДНОГО ТИПУ з ФІКСОВАНОЮ довжиною
-	//var імʼя [розмір]тип
-	var numbers [5]int = [5]int{1, 2, 3, 4, 5}
-	fmt.Println(&numbers[0])
-	fmt.Println(&numbers[1])
-	fmt.Println(&numbers[2])
-	fmt.Println(&numbers[3])
-	fmt.Println(&numbers[4])
+	//слайси - це динамічний масив, який може змінювати свій розмір під час виконання програми
+	//var імʼя []тип
+	// var numbers []int
 
-	var fruits [4]string = [4]string{"apple", "orange", "banana", "peach"}
-	fmt.Println(&fruits[0])
-	fmt.Println(&fruits[1])
-	fmt.Println(&fruits[2])
-	fmt.Println(&fruits[3])
+	arr := [5]int{1,2,3,4,5}
 
-	names := [...]string{"Ann", "Bogdan", "Vika"}
-	names[2] = "Kleo"
-	fmt.Println(names)
-	fmt.Println(len(names))
+	slice1 := arr[1:4]
+	fmt.Println(slice1)
+
+	slice2 := arr[:3]
+	slice3 := arr[2:]
+	slice4 := arr[:]
+	fmt.Println(slice2)
+	fmt.Println(slice3)
+	fmt.Println(slice4)
 }
