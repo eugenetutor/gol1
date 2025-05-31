e5c1349911215444b140297989f2a0a1d4d49c27
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-31 14:09:00 +0300

    struct. init

--- main.go
+++ main.go
@@ -3,14 +3,38 @@ package main
 import (
 	"fmt"
 )
-//custom type
-type names string
-type km float64
+
+//структури - це визначений користувачем тип, який дозволяє групувати, комбінувати елементи різних типів в один тип. 
+//struct is value type. NOT REFERENCE TYPE
+type Student struct{
+	Name string
+	Age int 
+	Group string
+	Avarage float64
+}
 
 func main() {
-	var jr1 names = "Loid"
-	var jr2 names = "Nina"
-	fmt.Println(jr1)
-	fmt.Println(jr2)
-	var section1 km = 54.5
+	student1 := Student{
+		Name: "Alex",
+		Age: 13,
+		Group: "IT-101",
+		Avarage: 8.5,
+	}
+	student2 := Student{
+		Name: "Maria",
+		Age: 12,
+		//group, avg -> null
+	}
+
+	var student3 Student
+	student3.Name = "Andrew"
+	student3.Age = 18
+	student3.Group = "IT-102"
+	student3.Avarage = 9.2
+
+	fmt.Printf("Student 1: %+v\n", student1)
+	fmt.Printf("Student 2: %+v\n", student2)
+	fmt.Printf("Student 2: %+v\n", student3)
+
+	fmt.Printf("Student1 name: %s\n", student1.Name)
 }
