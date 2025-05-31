96600ddbc69e7ed4c0c09045bc13c9cd12bf80a1
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-31 13:47:32 +0300

    custom type

--- main.go
+++ main.go
@@ -3,33 +3,14 @@ package main
 import (
 	"fmt"
 )
+//custom type
+type names string
+type km float64
 
 func main() {
-	//мапа - це невпорядкована колекція пар ключ-значень, де ключі УНІКАЛЬНІ
-
-	// var name map[KeyType]ValueType
-	// var scores map[string]int
-
-	userAges := map[string]int{
-		"Ann": 25,
-		"Bohdan": 30,
-		"Vika": 22,
-	}
-	fmt.Println(userAges)
-	contacts := make(map[string]string)
-
-	contacts["Mike"] = "+380505005050"
-	contacts["Natalie"] = "+380666006060"
-	fmt.Println(contacts)
-
-	mikePhone, exists := contacts["Leo"]
-	if exists {
-		fmt.Println(mikePhone)
-	} else {
-		fmt.Println("nothing")
-	}
-	
-	delete(contacts, "Mike")
-	fmt.Println(contacts)
+	var jr1 names = "Loid"
+	var jr2 names = "Nina"
+	fmt.Println(jr1)
+	fmt.Println(jr2)
+	var section1 km = 54.5
 }
-
