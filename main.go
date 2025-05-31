28f0a8c2ff5542fbc1526b3d76a5b912c838862f
Eugene Demenko <eugenekiberone@gmail.com>
2025-05-31 14:35:39 +0300

    value vs reference. BEFORE

--- main.go
+++ main.go
@@ -13,38 +13,10 @@ type Student struct{
 	Avarage float64
 }
 
-type School struct{
-	Students []Student
-}
-
 func main() {
-	student1 := Student{
-		Name: "Alex",
-		Age: 13,
-		Group: "IT-101",
-		Avarage: 8.5,
-	}
-	student2 := Student{
-		Name: "Maria",
-		Age: 12,
-		//group, avg -> null
-	}
-
-	var student3 Student
-	student3.Name = "Andrew"
-	student3.Age = 18
-	student3.Group = "IT-102"
-	student3.Avarage = 9.2
-
-	fmt.Printf("Student 1: %+v\n", student1)
-	fmt.Printf("Student 2: %+v\n", student2)
-	fmt.Printf("Student 2: %+v\n", student3)
-
-	fmt.Printf("Student1 name: %s\n", student1.Name)
- 
-	studentsSlice := []Student{student1, student2, student3}
-	school := School{
-		Students: studentsSlice,
-	}
-	fmt.Printf("School: %v\n", school)
+	a := Student{ Name: "Ann", Age: 25}
+	b := a
+	b.Name = "Bob"
+	fmt.Println(a.Name)
+	fmt.Println(b.Name)
 }
